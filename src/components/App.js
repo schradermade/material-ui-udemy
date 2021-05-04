b4adae95e8e57912eb6ffb8ad1809a19c9a7d28a
Nathan Schrader <schradermade@nathans-mbp.lan>
2021-05-04 10:06:15 -0700

    add file structure for Material-UI Udemy course

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  ThemeProvider } from '@material-ui/core/styles';
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello
+    </ThemeProvider>
+  );
+}
+
+export default App;
