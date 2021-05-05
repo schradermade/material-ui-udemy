11e787e16c61909f31aac455b5ef8a6f3d4a2c8c
Nathan Schrader <schradermade@nathans-mbp.lan>
2021-05-05 15:24:28 -0700

    implement useEffet hook to set correct value for Active Tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -7,6 +7,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab'
 import logo from '../../assets/logo.svg'
 import Button from '@material-ui/core/Button'
+import { Link } from "react-router-dom"
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -55,6 +56,22 @@ export default function Header(props) {
     setValue(value)
   }
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1)
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2)
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3)
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4)
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5)
+    }
+  }, [value]);
+
   return (
     <>
       <ElevationScroll>
@@ -66,11 +83,11 @@ export default function Header(props) {
               onChange={handleChange} 
               className={classes.tabContainer}
               indicatorColor="primary">
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab}label="Contact Us" />
+              <Tab className={classes.tab} component={Link} to="/" label="Home" />
+              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
+              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
+              <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
+              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
             </Tabs>
             <Button variant="contained" color="secondary"
             className={classes.button}>
