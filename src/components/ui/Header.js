33db89cc6c2bfd407a71e9d5ca96e856c3069c9a
Nathan Schrader <schradermade@nathans-mbp.lan>
2021-05-05 14:32:37 -0700

    add style to the Active Tab in NavBar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -49,6 +49,11 @@ const useStyles = makeStyles(theme => ({
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
 
   return (
     <>
@@ -56,7 +61,11 @@ export default function Header(props) {
         <AppBar position='fixed'>
           <Toolbar disableGutters>
             <img alt="company logo" src={logo} className={classes.logo}/>
-            <Tabs className={classes.tabContainer}>
+            <Tabs 
+              value={value} 
+              onChange={handleChange} 
+              className={classes.tabContainer}
+              indicatorColor="primary">
               <Tab className={classes.tab} label="Home" />
               <Tab className={classes.tab} label="Services" />
               <Tab className={classes.tab} label="The Revolution" />
